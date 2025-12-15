942218fb5d8a95d91ba338000aff11f1c55da1a8
vladimirmitnovitskiy <harrellprmime@gmail.com>
2025-12-15 15:03:18 +0300

    add "test"

--- src/rewrtTest/walker_circuit.py
+++ src/rewrtTest/walker_circuit.py
@@ -33,3 +33,25 @@ class walker:
                 return event
     def __str__(self):
         return f"{self.data}"
+    
+#пример 1 нормальная работа
+w = walker([("A",0.7),("B",0.2),("C",0.1)])
+print(w.get_random())
+
+#пример 2 когда сумма вероятностей не равна 1
+try:
+    w = walker([("A",0.1), ("B", 0.2)])
+except ValueError as e:
+    print(e)
+
+#пример 3 когда когда ввели отрицательное число
+try:
+    w = walker([("A", -0.1), ("B", 0.1)])
+except ValueError as e:
+    print(e)
+
+#пример 4 когда список пустой
+try:
+    w = walker([])
+except ValueError as e:
+    print(e)
